2fac784e5fcf47b82a4b4d7ce2545013aff0f115
Carl <carlbrainegebe@gmail.com>
2021-07-25 10:18:30 -0500

    commit tree 8eea316f3ecd59b5044d857e93dadce71e4c24f9

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,44 @@
+# require_relative './dance_module'
+# require_relative './meta_dancing_module'
+# require_relative './fancy_dance.rb'
+
+# class Dancer
+    
+#     extend MetaDancing
+#     include Dance
+#     attr_accessor :name
+
+#     def initialize(name)
+#         @name = name
+#     end
+
+
+#     extend FancyDance::ClassMethods
+#     include FancyDance::InstanceMethods
+
+# end
+
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+
+class Dancer
+  include Dance
+  extend MetaDancing
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
+
+# with namespaced modules
+# require_relative './fancy_dance'
+# class Dancer
+#   include FancyDance::InstanceMethods
+#   extend FancyDance::ClassMethods
+#   attr_accessor :name
+
+#   def initialize(name)
+#     @name = name
+#   end
+# end
